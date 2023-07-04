54f64612e7895f3a160d7a8e41912cab5b9f3a29
michaelmmmz <138337610+michaelmmmz@users.noreply.github.com>
2023-07-04 14:05:26 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import NodeMailer from "nodemailer";
+import bodyParser from "body-parser";
+import { useState } from "react";
 
 const app = express();
 const port = process.env.PORT || 3000; //add your port here
@@ -14,6 +17,9 @@ const stripe = Stripe(SECRET_KEY, { apiVersion: "2022-11-15" });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.json("Hello Mane");
@@ -36,4 +42,63 @@ app.post("/create-payment-intent", async (req, res) => {
     console.log(e.message);
     res.json({ error: e.message });
   }
+});
+
+app.post("/nodemail", async (req, res) => {
+  console.log("accessed");
+  const words = res.req.headers.value.split(" ");
+  console.log(words[0]);
+  console.log(words[1]);
+
+  let products = res.req.body;
+
+  const transporter = NodeMailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "apptestbymzv1@gmail.com",
+      pass: "qfwzaafiwmiqyoup",
+    },
+  });
+
+  let output = `
+        <h2>Thank you For Shopping With Double Trouble</h2>
+<h3>Order Confirmation: </h3>
+<h4>Shipping Address: ${words[2]}</h4>
+    `;
+
+  products.forEach((product) => {
+    console.log(product.name);
+    output += `
+    <div style="display: flex">
+  <img src="${product.imageURL}" alt="Product Image" width="250" height="250">
+  <div>
+    <div style="display: block">
+      <div>${product.name}</div>
+      <div>${product.price}</div>
+      <div>${product.size}</div>
+        <div>${product.quantity}</div>
+      <div>${product.participantName}</div>
+      <div>${product.participantGrade}</div>
+      <div>${product.jerseySize}</div>
+      <div>${product.jerseyShortSize}</div>
+    </div>
+  </div>
+</div>
+    `;
+  });
+
+  const options = {
+    from: "apptestbymzv1@gmail.com",
+    to: [words[0], words[1], "apptestbymz1@gmail.com"],
+    subject:
+      "Order Confirmation for " + words[0] + " - Account Email: " + words[1],
+    html: output,
+  };
+
+  transporter.sendMail(options, (error, info) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log("Sent: " + info.response);
+  });
 });
